1128183de6021b13f84d85a67907434854b1608b
tanea joshi <taneajoshi@gmail.com>
2023-05-09 22:42:50 +0530

    added ingredients to the recipe module and showed them in the frontend

--- src/app/pages/recipes/recipe.model.ts
+++ src/app/pages/recipes/recipe.model.ts
@@ -1,12 +1,16 @@
+import { Ingredient } from "src/app/shared/ingredient.model";
+
 //We can define how a recipe should look like throughout the application.
 export class Recipe {
     public name: string;
     public description: string;
     public imageUrl: string;
+    public Ingredients: Ingredient[];
 
-    constructor(name: string, desc: string, imageUrl: string) {
+    constructor(name: string, desc: string, imageUrl: string, ingredients: Ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imageUrl = imageUrl;
+        this.Ingredients = ingredients;
     }
 }
--- src/app/services/recipe/recipe.service.ts
+++ src/app/services/recipe/recipe.service.ts
@@ -1,14 +1,29 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from 'src/app/pages/recipes/recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
+  recipeSelected = new EventEmitter < Recipe > ();
   private recipes: Recipe[] = [
-    new Recipe('test recipe', 'This is description for our test recipe', 'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg'),
-    new Recipe('test recipe 2', 'This is description 2 for our test recipe', 'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg'),
+    new Recipe('test recipe',
+      'This is description for our test recipe',
+      'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient ('Yogurt', 1)
+      ]
+    ),
+    new Recipe(
+      'test recipe 2',
+      'This is description 2 for our test recipe',
+      'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg',
+      [
+        new Ingredient('French Fries', 20),
+        new Ingredient ('Buns', 2)
+      ]),
   ];
 
   getRecipes() {
