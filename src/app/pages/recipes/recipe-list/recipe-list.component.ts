83fb54256f54ea12dfa704a6c93417868b84e915
tanea joshi <taneajoshi@gmail.com>
2023-05-25 14:35:25 +0530

    added image preview for the image url added by the user in the edit recipe form. Unsubsribe the recipeChange observable

--- src/app/pages/recipes/recipe-list/recipe-list.component.ts
+++ src/app/pages/recipes/recipe-list/recipe-list.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from 'src/app/services/recipe/recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   constructor(private recipeService: RecipeService) {}
   recipes: Recipe[];
+  recipeChangedSubscription: Subscription;
 
   ngOnInit() {
-    this.recipeService.recipesChanged.subscribe(
+    this.recipeChangedSubscription = this.recipeService.recipesChanged.subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
       }
     );
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  ngOnDestroy() {
+    this.recipeChangedSubscription.unsubscribe();
   }
 }
