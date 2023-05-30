50b8ab98fb9090cab94a1c40437c5fe3d4cdfeb4
tanea joshi <taneajoshi@gmail.com>
2023-05-31 00:05:35 +0530

    added login service and subscribed it in the auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -11,6 +11,8 @@ import { finalize, from } from 'rxjs';
 
 export class AuthComponent implements OnInit {
   isLoginMode = true;
+  emailError = '';
+  passwordError = '';
   constructor(private authService: AuthService, private spinner: NgxSpinnerService){}
   ngOnInit(): void {
     document.body.classList.add('bg-light');
@@ -20,35 +22,32 @@ export class AuthComponent implements OnInit {
     this.isLoginMode = !this.isLoginMode;
   }
 
-  // onSubmit(authForm: NgForm) {
-  //   const email = authForm.value.email;
-  //   const password = authForm.value.password;
-  //   from(this.spinner.show())
-  //   .pipe(finalize(() => this.spinner.hide()))
-  //   this.authService.signUp(email, password).subscribe(data => {
-  //     console.log(data);
-  //   },
-  //   error => {
-  //     console.log(error);
-  //   });
-  //   authForm.reset;
-  // }
-
   onSubmit(authForm: NgForm) {
     const email = authForm.value.email;
     const password = authForm.value.password;
     from(this.spinner.show())
+    if(this.isLoginMode){
+      //Code for login
+      this.authService.login(email, password)
       .pipe(finalize(() => this.spinner.hide()))
-      .subscribe(() => {
-        this.authService.signUp(email, password).subscribe(
-          data => {
+        .subscribe(data => {
             console.log(data);
           },
-          error => {
-            console.log(error);
+          errorMessage => {
+            this.passwordError = errorMessage;
           }
         );
-      });
+    } else {
+      //Code for SignUp
+      this.authService.signUp(email, password)
+        .pipe(finalize(() => this.spinner.hide()))
+        .subscribe(data => {
+            console.log(data);
+          },
+          errorMessage => {
+            this.emailError = errorMessage;
+          });
+    }
     authForm.reset();
   }
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 import { authResponseData } from 'src/app/interfaces/auth-response-data';
 
 @Injectable({
@@ -8,6 +9,8 @@ import { authResponseData } from 'src/app/interfaces/auth-response-data';
 export class AuthService {
 
   constructor(private http: HttpClient){}
+
+  //SignUp
   signUp(email: string, password: string) {
    return this.http
     .post<authResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDjSsnl0kajSC75WX710Spqm08izPL2FF8', {
@@ -15,6 +18,40 @@ export class AuthService {
       password: password,
       //As per firebase Api Docs returnSecureToken should always be true
       returnSecureToken: true,
-    })
+    }).pipe(
+      catchError(errorRes => {
+        let errorMessage = "Something went wrong!";
+        if(!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch(errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'This email already exists.'
+        }
+        return throwError(errorMessage);
+      })
+    )
+  }
+
+  //Login
+  login(email: string, password: string) {
+    return this.http.post<authResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDjSsnl0kajSC75WX710Spqm08izPL2FF8', {
+      email: email,
+      password: password,
+      //As per firebase Api Docs returnSecureToken should always be true
+      returnSecureToken: true,
+    }).pipe(
+      catchError(errorRes => {
+        let errorMessage = "Something went wrong!";
+        if(!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch(errorRes.error.error.message) {
+          case 'INVALID_PASSWORD':
+            errorMessage = 'Password is invalid.'
+        }
+        return throwError(errorMessage);
+      })
+    )
   }
 }
